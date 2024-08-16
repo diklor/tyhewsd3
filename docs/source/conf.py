3f2bd861092bd45fcec7d172ae2a1465db02028e
diklor <guyueege8090@gmail.com>
2024-08-16 09:07:31 +0300

    2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,23 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_permalinks_icon = '<span>#</span>'
+html_theme = 'sphinxawesome_theme'
+html_theme_options = {
+    'analytics_anonymize_ip': True,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
